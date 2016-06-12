0ead916195bf49ff44a3082ce6e973c0eadc0057
Marin Luketin <marin.luketin@gmail.com>
2016-06-12 13:24:46 +0200

    Add ScalaToJavaConverter object

---
+++ src/main/scala/com/scalaq/survey/ScalaToJavaConverter.scala
@@ -0,0 +1,33 @@
+package com.scalaq.survey
+
+import java.util
+
+import com.scalaq.survey.model.answer.Answer
+import com.scalaq.survey.model.question.Question
+
+import scalaq.persistence
+
+
+object ScalaToJavaConverter {
+
+  /**
+    * converts sequence of model questions into list of persistance question
+    *
+    * @param questions
+    * @return
+    */
+  def scalaToJavaQuestions(questions: Seq[Question]): util.List[persistence.Question] = {
+    scala.collection.JavaConversions.seqAsJavaList(for (q <- questions) yield q.getPersistanceQuestion())
+  }
+
+  /**
+    * converts sequence of model answers into list of persistence answers
+    *
+    * @param answers
+    * @return
+    */
+  def scalaToJavaAnswers(answers: Seq[Answer]): util.List[persistence.Answer] = {
+    scala.collection.JavaConversions.seqAsJavaList(for (a <- answers) yield a.getPersistenceAnswer())
+  }
+
+}
