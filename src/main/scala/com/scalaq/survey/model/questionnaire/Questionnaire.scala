89ca57de88ece533b5cf0f8cc725e98ff482cafd
Marin Luketin <marin.luketin@gmail.com>
2016-06-12 13:24:23 +0200

    Add getPersistenceQuestionnaire method in model Questionnaire

--- src/main/scala/com/scalaq/survey/model/questionnaire/Questionnaire.scala
+++ src/main/scala/com/scalaq/survey/model/questionnaire/Questionnaire.scala
@@ -1,6 +1,9 @@
 package com.scalaq.survey.model.questionnaire
 
+import com.scalaq.survey.ScalaToJavaConverter
 import com.scalaq.survey.model.question.Question
+
+import scalaq.persistence
 
 /**
   *
@@ -8,7 +11,21 @@ import com.scalaq.survey.model.question.Question
   * @param description optional description for questionnaire
   * @param questions sequence of questions that are in questionnaire
   */
-case class Questionnaire(name: String, description: Option[String], questions: Seq[Question])
+case class Questionnaire(name: String, description: Option[String], questions: Seq[Question]) {
+
+  /**
+    * converts model questionnaire into persistence questionnaire.
+    * (Note: this is not the same persistence object as you can find in database if you have already saved model Questionnaire)
+    * @return
+    */
+  def getPersistanceQuestionnaire(): persistence.Questionnaire = {
+    new persistence.Questionnaire()
+      .setName(name)
+      .setDescription(if (description == None) "" else description.get)
+      .setQuestions(ScalaToJavaConverter.scalaToJavaQuestions(questions))
+  }
+
+}
 
 
 
